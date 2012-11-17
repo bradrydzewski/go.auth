c107a4421bc8a1a746a071086ff2bcaeec8f75f0
Brad Rydzewski <brad.rydzewski@gmail.com>
2012-11-16 22:19:00 -0700

    preliminary version of twitter working

--- twitter.go
+++ twitter.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 )
 
-type TwitterUser struct { }
+type TwitterUser struct {
+	UserId string `json:"screen_name"`
+}
 
-func (u *TwitterUser) Id()       string { return "test" }
+func (u *TwitterUser) Id()       string { return u.UserId }
 func (u *TwitterUser) Provider() string { return "twitter.com" }
-func (u *TwitterUser) Name()     string { return "test" }
+func (u *TwitterUser) Name()     string { return u.UserId }
 func (u *TwitterUser) Email()    string { return "" }
-func (u *TwitterUser) Link()     string { return "https://www.twitter.com/test" }
+func (u *TwitterUser) Link()     string { return "https://www.twitter.com/" + u.UserId }
 func (u *TwitterUser) Picture()  string { return "" }
 func (u *TwitterUser) Org()      string { return "" }
 
@@ -25,10 +27,10 @@ type TwitterProvider struct {
 // NewTwitterProvider allocates and returns a new BitbucketProvider.
 func NewTwitterProvider(key, secret, callback string) *TwitterProvider {
 	twitter := TwitterProvider{}
-	twitter.AuthorizeUrl = "https://api.twitter.com/oauth/authorize" //"https://api.twitter.com/oauth/authenticate"
+	twitter.AuthorizeUrl = "https://api.twitter.com/oauth/authorize"
 	twitter.RequestToken = "https://api.twitter.com/oauth/request_token"
 	twitter.AccessToken =  "https://api.twitter.com/oauth/access_token"
-	twitter.UserResourceUrl = "http://api.twitter.com/1.1/users/show.json"
+	twitter.UserResourceUrl = "https://api.twitter.com/1.1/account/settings.json" //"http://api.twitter.com/1.1/users/show.json"
 
 	twitter.CallbackUrl = callback
 	twitter.ConsumerKey = key
@@ -55,7 +57,10 @@ func (self *TwitterProvider) GetAuthenticatedUser(r *http.Request) (User, error)
 		return nil, err
 	}
 
-	println("TOKEN:"+token)
-	println("SECRET:"+secret)
-	return &TwitterUser{}, err
+	// get the Bitbucket User details
+	user := TwitterUser{}
+	if err := self.OAuth1Mixin.GetAuthenticatedUser(self.UserResourceUrl, token, secret, &user); err != nil {
+		return nil, err
+	}
+	return &user, err
 }
